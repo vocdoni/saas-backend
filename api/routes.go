058a873efd9b5fd29a7af41c62afc0f92133b7c1
Lucas Menéndez <hi@lucasmenendez.me>
2024-07-30 14:48:56 +0200

    routes comments

--- api/routes.go
+++ api/routes.go
@@ -2,9 +2,16 @@ package api
 
 const (
 	// auth routes
+
+	// POST /auth/refresh to refresh the JWT token
 	authRefresTokenEndpoint = "/auth/refresh"
-	authLoginEndpoint       = "/auth/login"
+	// POST /auth/login to login and get a JWT token
+	authLoginEndpoint = "/auth/login"
+
 	// user routes
-	usersEndpoint              = "/users"
+
+	// POST /users to register a new user
+	usersEndpoint = "/users"
+	// GET /users/address to get the address of the current user
 	currentUserAddressEndpoint = "/users/address"
 )
