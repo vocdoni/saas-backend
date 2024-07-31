44fad32ff7d0679bb32909692147cb5e16e2d783
Lucas Menéndez <hi@lucasmenendez.me>
2024-07-31 09:16:27 +0200

    initial db integration

---
+++ db/mongo/mongo.go
@@ -0,0 +1,178 @@
+package mongo
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"os"
+	"sync"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+	"go.vocdoni.io/dvote/log"
+)
+
+// MongoStorage uses an external MongoDB service for stoting the user data and election details.
+type MongoStorage struct {
+	client   *mongo.Client
+	keysLock sync.RWMutex
+
+	users *mongo.Collection
+}
+
+type Options struct {
+	MongoURL string
+	Database string
+}
+
+func New(url, database string) (*MongoStorage, error) {
+	var err error
+	ms := &MongoStorage{}
+	if url == "" {
+		return nil, fmt.Errorf("mongo URL is not defined")
+	}
+	if database == "" {
+		return nil, fmt.Errorf("mongo database is not defined")
+	}
+	log.Infow("connecting to mongodb", "url", url, "database", database)
+	// preparing connection
+	opts := options.Client()
+	opts.ApplyURI(url)
+	opts.SetMaxConnecting(200)
+	timeout := time.Second * 10
+	opts.ConnectTimeout = &timeout
+	// create a new client with the connection options
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	defer cancel()
+	client, err := mongo.Connect(ctx, opts)
+	if err != nil {
+		return nil, fmt.Errorf("cannot connect to mongodb: %w", err)
+	}
+	// check if the connection is successful
+	ctx, cancel2 := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel2()
+	err = client.Ping(ctx, readpref.Primary())
+	if err != nil {
+		return nil, fmt.Errorf("cannot connect to mongodb: %w", err)
+	}
+	// init the collections
+	ms.client = client
+	ms.users = client.Database(database).Collection("users")
+	// if reset flag is enabled, Reset drops the database documents and recreates indexes
+	// else, just createIndexes
+	if reset := os.Getenv("VOCDONI_MONGO_RESET_DB"); reset != "" {
+		err := ms.Reset()
+		if err != nil {
+			return nil, err
+		}
+	} else {
+		err := ms.createIndexes()
+		if err != nil {
+			return nil, err
+		}
+	}
+	return ms, nil
+}
+
+func (ms *MongoStorage) Close() {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	if err := ms.client.Disconnect(ctx); err != nil {
+		log.Warn(err)
+	}
+}
+
+func (ms *MongoStorage) createIndexes() error {
+	ctx, cancel := context.WithTimeout(context.Background(), 60*time.Second)
+	defer cancel()
+
+	// Create an index for the 'addresses' field on users
+	addressesIndexModel := mongo.IndexModel{
+		Keys:    bson.D{{Key: "addresses", Value: 1}}, // 1 for ascending order
+		Options: nil,
+	}
+	if _, err := ms.users.Indexes().CreateOne(ctx, addressesIndexModel); err != nil {
+		return fmt.Errorf("failed to create index on addresses for users: %w", err)
+	}
+
+	return nil
+}
+
+func (ms *MongoStorage) Reset() error {
+	log.Infof("resetting database")
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	if err := ms.users.Drop(ctx); err != nil {
+		return err
+	}
+	if err := ms.createIndexes(); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (ms *MongoStorage) String() string {
+	const contextTimeout = 30 * time.Second
+	ms.keysLock.RLock()
+	defer ms.keysLock.RUnlock()
+
+	ctx, cancel := context.WithTimeout(context.Background(), contextTimeout)
+	defer cancel()
+	cur, err := ms.users.Find(ctx, bson.D{{}})
+	if err != nil {
+		log.Warn(err)
+		return "{}"
+	}
+
+	ctx2, cancel2 := context.WithTimeout(context.Background(), contextTimeout)
+	defer cancel2()
+	var users UserCollection
+	for cur.Next(ctx2) {
+		var user User
+		err := cur.Decode(&user)
+		if err != nil {
+			log.Warn(err)
+		}
+		users.Users = append(users.Users, user)
+	}
+
+	data, err := json.Marshal(&Collection{users})
+	if err != nil {
+		log.Warn(err)
+	}
+	return string(data)
+}
+
+// Import imports a JSON dataset produced by String() into the database.
+func (ms *MongoStorage) Import(jsonData []byte) error {
+	ms.keysLock.RLock()
+	defer ms.keysLock.RUnlock()
+
+	log.Infof("importing database")
+	var collection Collection
+	err := json.Unmarshal(jsonData, &collection)
+	if err != nil {
+		return err
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 120*time.Second)
+	defer cancel()
+
+	// Upsert Users
+	log.Infow("importing users", "count", len(collection.Users))
+	for _, user := range collection.Users {
+		filter := bson.M{"_id": user.ID}
+		update := bson.M{"$set": user}
+		opts := options.Update().SetUpsert(true)
+		_, err := ms.users.UpdateOne(ctx, filter, update, opts)
+		if err != nil {
+			log.Warnw("error upserting user", "err", err, "user", user.ID)
+		}
+	}
+
+	log.Infof("imported database!")
+	return nil
+}
--- go.mod
+++ go.mod
@@ -7,6 +7,8 @@ require (
 	github.com/go-chi/cors v1.2.1
 	github.com/go-chi/jwtauth/v5 v5.3.1
 	github.com/lestrrat-go/jwx/v2 v2.0.20
+	github.com/vocdoni/vote-frame v0.0.0-20240731065052-8ec7ec5d1cdf
+	go.mongodb.org/mongo-driver v1.14.0
 	go.vocdoni.io/dvote v1.10.2-0.20240726114655-b510ac8a7e42
 )
 
@@ -14,13 +16,15 @@ require (
 	github.com/btcsuite/btcd/btcec/v2 v2.3.2 // indirect
 	github.com/cometbft/cometbft v1.0.0-alpha.1 // indirect
 	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.2.0 // indirect
-	github.com/ethereum/go-ethereum v1.14.0 // indirect
+	github.com/ethereum/go-ethereum v1.14.3 // indirect
 	github.com/glendc/go-external-ip v0.1.0 // indirect
 	github.com/go-kit/log v0.2.1 // indirect
 	github.com/go-logfmt/logfmt v0.6.0 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
+	github.com/golang/snappy v0.0.5-0.20220116011046-fa5810519dcb // indirect
 	github.com/holiman/uint256 v1.2.4 // indirect
 	github.com/iden3/go-iden3-crypto v0.0.13 // indirect
+	github.com/klauspost/compress v1.17.8 // indirect
 	github.com/lestrrat-go/blackmagic v1.0.2 // indirect
 	github.com/lestrrat-go/httpcc v1.0.1 // indirect
 	github.com/lestrrat-go/httprc v1.0.4 // indirect
@@ -29,15 +33,22 @@ require (
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mfridman/interpolate v0.0.2 // indirect
+	github.com/montanaflynn/stats v0.7.1 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pressly/goose/v3 v3.20.0 // indirect
 	github.com/rs/zerolog v1.33.0 // indirect
 	github.com/segmentio/asm v1.2.0 // indirect
 	github.com/sethvargo/go-retry v0.2.4 // indirect
+	github.com/xakep666/mongo-migrate v0.3.2 // indirect
+	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
+	github.com/xdg-go/scram v1.1.2 // indirect
+	github.com/xdg-go/stringprep v1.0.4 // indirect
+	github.com/youmark/pkcs8 v0.0.0-20201027041543-1326539a0a0a // indirect
 	go.uber.org/multierr v1.11.0 // indirect
 	go.vocdoni.io/proto v1.15.8 // indirect
 	golang.org/x/crypto v0.22.0 // indirect
 	golang.org/x/sync v0.7.0 // indirect
 	golang.org/x/sys v0.19.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
 	google.golang.org/protobuf v1.34.0 // indirect
 )
