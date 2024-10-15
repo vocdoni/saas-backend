8d058e44081fbedb54c1c1312c8ee3b052bdfe59
emmdim <emmdim@users.noreply.github.com>
2024-10-16 00:07:39 +0200

    WIP

--- api/subscriptions.go
+++ api/subscriptions.go
@@ -1,7 +1,15 @@
 package api
 
 import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"net/http"
+	"os"
+
+	"github.com/stripe/stripe-go/v80"
+	"github.com/stripe/stripe-go/v80/webhook"
+	"go.vocdoni.io/dvote/log"
 )
 
 // getSubscriptionsHandler handles the request to get the subscriptions of an organization.
@@ -15,4 +23,55 @@ func (a *API) getSubscriptionsHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	// send the subscriptions back to the user
 	httpWriteJSON(w, subscriptions)
+}
+
+// HandleWebhook handles the incoming webhook event from Stripe.
+// It takes the API data and signature as input parameters and returns the session ID and an error (if any).
+// The request body and Stripe-Signature header are passed to ConstructEvent, along with the webhook signing key.
+// If the event type is "checkout.session.completed", it unmarshals the event data into a CheckoutSession struct
+// and returns the session ID. Otherwise, it returns an empty string.
+func (a *API) HandleWebhook(w http.ResponseWriter, r *http.Request) {
+
+	const MaxBodyBytes = int64(65536)
+	r.Body = http.MaxBytesReader(w, r.Body, MaxBodyBytes)
+	payload, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+
+		log.Warnw("Stripe Webhook: Error reading request body: %v\n", err)
+		w.WriteHeader(http.StatusServiceUnavailable)
+		return
+	}
+
+	event := stripe.Event{}
+
+	if err := json.Unmarshal(payload, &event); err != nil {
+		log.Warnw("Stripe Webhook: error while parsing basic request. %v\n", err.Error())
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	endpointSecret := "whsec_QHZlw9fOPECQKGsHeXFTLpu36G4ko75b"
+	signatureHeader := r.Header.Get("Stripe-Signature")
+	event, err = webhook.ConstructEvent(payload, signatureHeader, endpointSecret)
+	if err != nil {
+		log.Warnw("Stripe Webhook: Webhook signature verification failed. %v\n", err)
+		w.WriteHeader(http.StatusBadRequest) // Return a 400 error on a bad signature
+		return
+	}
+
+	// Unmarshal the event data into an appropriate struct depending on its Type
+	switch event.Type {
+	case "customer.subscription.created":
+		var subscription stripe.Subscription
+		err := json.Unmarshal(event.Data.Raw, &subscription)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error parsing webhook JSON: %v\n", err)
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		log.Warnf("Subscription created for %d.", subscription.ID)
+		log.Warnf("Subscription plan for %s.", subscription.Items.Data[0].Plan.ID)
+		// Then define and call a func to handle the successful attachment of a PaymentMethod.
+		// handleSubscriptionCreated(subscription)
+	}
 }
