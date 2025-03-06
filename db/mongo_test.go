71a481659482eb5ca0adf52099e648bdc40bc03a
p4u <pau@dabax.net>
2025-03-06 10:46:19 +0100

    fix census membership test
    Signed-off-by: p4u <pau@dabax.net>

--- db/mongo_test.go
+++ db/mongo_test.go
@@ -9,6 +9,40 @@ import (
 	"github.com/vocdoni/saas-backend/test"
 )
 
+// startTestDB starts a MongoDB container for testing and returns a new MongoStorage instance.
+func startTestDB(t *testing.T) *MongoStorage {
+	ctx := context.Background()
+	// start a MongoDB container for testing
+	dbContainer, err := test.StartMongoContainer(ctx)
+	if err != nil {
+		t.Fatalf("failed to start MongoDB container: %v", err)
+	}
+
+	// ensure the container is stopped when the test finishes
+	t.Cleanup(func() { _ = dbContainer.Terminate(ctx) })
+
+	// get the MongoDB connection string
+	mongoURI, err := dbContainer.Endpoint(ctx, "mongodb")
+	if err != nil {
+		t.Fatalf("failed to get MongoDB endpoint: %v", err)
+	}
+
+	plans, err := ReadPlanJSON()
+	if err != nil {
+		t.Fatalf("failed to read plan JSON: %v", err)
+	}
+
+	testDB, err := New(mongoURI, test.RandomDatabaseName(), plans)
+	if err != nil {
+		t.Fatalf("failed to create new MongoDB connection: %v", err)
+	}
+
+	// ensure the database is closed when the test finishes
+	t.Cleanup(func() { testDB.Close() })
+
+	return testDB
+}
+
 var db *MongoStorage
 
 func TestMain(m *testing.M) {
