8cd9f4cbfd825d8a57048a264ab69edbbf166d65
p4u <pau@dabax.net>
2025-03-07 13:16:58 +0100

    add helper functions to make the api tests easier
    Signed-off-by: p4u <pau@dabax.net>

--- api/api_test.go
+++ api/api_test.go
@@ -1,19 +1,28 @@
 package api
 
 import (
+	"bytes"
 	"context"
 	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
+	"net/url"
 	"os"
+	"path"
+	"strings"
 	"testing"
 	"time"
 
+	qt "github.com/frankban/quicktest"
 	"github.com/vocdoni/saas-backend/account"
 	"github.com/vocdoni/saas-backend/db"
+	"github.com/vocdoni/saas-backend/internal"
 	"github.com/vocdoni/saas-backend/notifications/smtp"
+	"github.com/vocdoni/saas-backend/subscriptions"
 	"github.com/vocdoni/saas-backend/test"
 	"go.vocdoni.io/dvote/apiclient"
+	"go.vocdoni.io/dvote/log"
 )
 
 type apiTestCase struct {
@@ -31,12 +40,14 @@ const (
 	testFirstName = "test"
 	testLastName  = "user"
 	testHost      = "0.0.0.0"
-	testPort      = 7788
 
 	adminEmail = "admin@test.com"
 	adminUser  = "admin"
 	adminPass  = "admin123"
 )
+
+// testPort is the port used for the API tests.
+var testPort int
 
 // testDB is the MongoDB storage for the tests. Make it global so it can be
 // accessed by the tests directly.
@@ -45,6 +56,11 @@ var testDB *db.MongoStorage
 // testMailService is the test mail service for the tests. Make it global so it
 // can be accessed by the tests directly.
 var testMailService *smtp.SMTPEmail
+
+func init() {
+	// set the test port
+	testPort = 40000 + internal.RandomInt(10000)
+}
 
 // testURL helper function returns the full URL for the given path using the
 // test host and port.
@@ -93,7 +109,9 @@ func pingAPI(endpoint string, retries int) error {
 // account with the Voconed private key and the API container endpoint. It
 // starts the API server and waits for it to start before running the tests.
 func TestMain(m *testing.M) {
-	ctx := context.Background()
+	log.Init("debug", "stdout", nil)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Minute)
+	defer cancel()
 	// start a MongoDB container for testing
 	dbContainer, err := test.StartMongoContainer(ctx)
 	if err != nil {
@@ -117,12 +135,6 @@ func TestMain(m *testing.M) {
 		panic(err)
 	}
 	defer testDB.Close()
-	// start the faucet container
-	faucetContainer, err := test.StartVocfaucetContainer(ctx)
-	if err != nil {
-		panic(err)
-	}
-	defer func() { _ = faucetContainer.Terminate(ctx) }()
 	// start the voconed container
 	apiContainer, err := test.StartVoconedContainer(ctx)
 	if err != nil {
@@ -176,6 +188,11 @@ func TestMain(m *testing.M) {
 	}); err != nil {
 		panic(err)
 	}
+	// Initialize the subscriptions service
+	subscriptionsService := subscriptions.New(&subscriptions.SubscriptionsConfig{
+		DB: testDB,
+	})
+
 	// start the API
 	New(&APIConfig{
 		Host:                testHost,
@@ -185,7 +202,8 @@ func TestMain(m *testing.M) {
 		Client:              testAPIClient,
 		Account:             testAccount,
 		MailService:         testMailService,
-		FullTransparentMode: false,
+		FullTransparentMode: true, // Use transparent mode for tests
+		Subscriptions:       subscriptionsService,
 	}).Start()
 	// wait for the API to start
 	if err := pingAPI(testURL(pingEndpoint), 5); err != nil {
@@ -193,4 +211,114 @@ func TestMain(m *testing.M) {
 	}
 	// run the tests
 	os.Exit(m.Run())
+}
+
+// request sends a request to the API service and returns the response body and status code.
+// The body is expected to be a JSON object or null.
+// If jwt is not empty, it will be sent as a Bearer token.
+func testRequest(t *testing.T, method, jwt string, jsonBody any, urlPath ...string) ([]byte, int) {
+	body, err := json.Marshal(jsonBody)
+	qt.Assert(t, err, qt.IsNil)
+	testURL := fmt.Sprintf("http://%s:%d", testHost, testPort)
+	u, err := url.Parse(testURL)
+	qt.Assert(t, err, qt.IsNil)
+	// Handle the case where the last path component contains query parameters
+	lastIndex := len(urlPath) - 1
+	if lastIndex >= 0 && strings.Contains(urlPath[lastIndex], "?") {
+		parts := strings.SplitN(urlPath[lastIndex], "?", 2)
+		urlPath[lastIndex] = parts[0]
+		u.Path = path.Join(u.Path, path.Join(urlPath...))
+		u.RawQuery = parts[1]
+	} else {
+		u.Path = path.Join(u.Path, path.Join(urlPath...))
+	}
+	headers := http.Header{}
+	if jwt != "" {
+		headers = http.Header{"Authorization": []string{"Bearer " + jwt}}
+	}
+	t.Logf("requesting %s %s", method, u.String())
+	req, err := http.NewRequest(method, u.String(), bytes.NewReader(body))
+	qt.Assert(t, err, qt.IsNil)
+	req.Header = headers
+	if method == http.MethodPost || method == http.MethodPut {
+		req.Header.Set("Content-Type", "application/json")
+	}
+	c := http.DefaultClient
+	resp, err := c.Do(req)
+	if err != nil {
+		t.Logf("http error: %v", err)
+	}
+	data, err := io.ReadAll(resp.Body)
+	if err != nil {
+		t.Logf("read error: %v", err)
+	}
+	return data, resp.StatusCode
+}
+
+// testCreateUser creates a new user with the given password and returns the JWT token.
+func testCreateUser(t *testing.T, password string) string {
+	n := internal.RandomInt(10000)
+	mail := fmt.Sprintf("%d%s", n, testEmail)
+
+	// Register a new user
+	userInfo := &UserInfo{
+		Email:     mail,
+		Password:  password,
+		FirstName: fmt.Sprintf("%d%s", n, testFirstName),
+		LastName:  fmt.Sprintf("%d%s", n, testLastName),
+	}
+	_, status := testRequest(t, http.MethodPost, "", userInfo, usersEndpoint)
+	qt.Assert(t, status, qt.Equals, http.StatusOK)
+
+	// Get the verification code from the email
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	mailBody, err := testMailService.FindEmail(ctx, mail)
+	qt.Assert(t, err, qt.IsNil)
+
+	// Extract the verification code using regex
+	mailCode := verificationCodeRgx.FindStringSubmatch(mailBody)
+	qt.Assert(t, len(mailCode) > 1, qt.IsTrue)
+
+	// Verify the user account
+	verification := &UserVerification{
+		Email: mail,
+		Code:  mailCode[1],
+	}
+	_, status = testRequest(t, http.MethodPost, "", verification, verifyUserEndpoint)
+	qt.Assert(t, status, qt.Equals, http.StatusOK)
+
+	// Login to get the JWT token
+	loginInfo := &UserInfo{
+		Email:    mail,
+		Password: password,
+	}
+	respBody, status := testRequest(t, http.MethodPost, "", loginInfo, authLoginEndpoint)
+	qt.Assert(t, status, qt.Equals, http.StatusOK)
+
+	// Extract the token from the response
+	var loginResp LoginResponse
+	err = json.Unmarshal(respBody, &loginResp)
+	qt.Assert(t, err, qt.IsNil)
+	qt.Assert(t, loginResp.Token, qt.Not(qt.Equals), "")
+
+	return loginResp.Token
+}
+
+// testCreateOrganization creates a new organization and returns the address.
+func testCreateOrganization(t *testing.T, jwt string) string {
+	orgName := fmt.Sprintf("org-%d", internal.RandomInt(10000))
+	orgInfo := &OrganizationInfo{
+		Type:    string(db.CompanyType),
+		Website: fmt.Sprintf("https://%s.com", orgName),
+	}
+	respBody, status := testRequest(t, http.MethodPost, jwt, orgInfo, organizationsEndpoint)
+	qt.Assert(t, status, qt.Equals, http.StatusOK)
+
+	var orgResp OrganizationInfo
+	err := json.Unmarshal(respBody, &orgResp)
+	qt.Assert(t, err, qt.IsNil)
+	qt.Assert(t, orgResp.Address, qt.Not(qt.Equals), "")
+
+	return orgResp.Address
 }
--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ toolchain go1.23.6
 
 require (
 	github.com/arnaucube/go-blindsecp256k1 v0.0.0-20211204171003-644e7408753f
+	github.com/docker/docker v27.1.1+incompatible
 	github.com/docker/go-connections v0.5.0
 	github.com/enriquebris/goconcurrentqueue v0.7.0
 	github.com/ethereum/go-ethereum v1.14.7
@@ -19,12 +20,13 @@ require (
 	github.com/spf13/pflag v1.0.5
 	github.com/spf13/viper v1.19.0
 	github.com/stripe/stripe-go/v81 v81.0.0
-	github.com/testcontainers/testcontainers-go v0.32.0
+	github.com/testcontainers/testcontainers-go v0.35.0
 	github.com/twilio/twilio-go v1.23.0
 	github.com/xlzd/gotp v0.1.0
 	go.mongodb.org/mongo-driver v1.14.0
 	go.vocdoni.io/dvote v1.10.2-0.20250211224900-db31f07b943c
 	go.vocdoni.io/proto v1.15.10
+	golang.org/x/crypto v0.32.0
 	google.golang.org/protobuf v1.36.4
 )
 
@@ -38,7 +40,6 @@ require (
 	github.com/DataDog/zstd v1.5.2 // indirect
 	github.com/Jorropo/jsync v1.0.1 // indirect
 	github.com/Microsoft/go-winio v0.6.2 // indirect
-	github.com/Microsoft/hcsshim v0.11.5 // indirect
 	github.com/VictoriaMetrics/metrics v1.24.0 // indirect
 	github.com/alecthomas/units v0.0.0-20240927000941-0f3dac36c52b // indirect
 	github.com/alexbrainman/goissue34681 v0.0.0-20191006012335-3fc7a47baff5 // indirect
@@ -67,11 +68,11 @@ require (
 	github.com/consensys/gnark-crypto v0.12.1 // indirect
 	github.com/containerd/cgroups v1.1.0 // indirect
 	github.com/containerd/containerd v1.7.18 // indirect
-	github.com/containerd/errdefs v0.1.0 // indirect
 	github.com/containerd/log v0.1.0 // indirect
+	github.com/containerd/platforms v0.2.1 // indirect
 	github.com/coreos/go-systemd/v22 v22.5.0 // indirect
 	github.com/cosmos/gogoproto v1.7.0 // indirect
-	github.com/cpuguy83/dockercfg v0.3.1 // indirect
+	github.com/cpuguy83/dockercfg v0.3.2 // indirect
 	github.com/crackcomm/go-gitignore v0.0.0-20241020182519-7843d2ba8fdf // indirect
 	github.com/crate-crypto/go-ipa v0.0.0-20240223125850-b1e8a79f509c // indirect
 	github.com/crate-crypto/go-kzg-4844 v1.0.0 // indirect
@@ -85,7 +86,6 @@ require (
 	github.com/dgraph-io/ristretto v0.1.2-0.20240116140435-c67e07994f91 // indirect
 	github.com/dgraph-io/ristretto/v2 v2.1.0 // indirect
 	github.com/distribution/reference v0.6.0 // indirect
-	github.com/docker/docker v27.0.3+incompatible // indirect
 	github.com/docker/go-units v0.5.0 // indirect
 	github.com/dustin/go-humanize v1.0.1 // indirect
 	github.com/elastic/gosigar v0.14.3 // indirect
@@ -362,7 +362,6 @@ require (
 	go.uber.org/multierr v1.11.0 // indirect
 	go.uber.org/zap v1.27.0 // indirect
 	go4.org v0.0.0-20230225012048-214862532bf5 // indirect
-	golang.org/x/crypto v0.32.0 // indirect
 	golang.org/x/exp v0.0.0-20241009180824-f66d83c29e7c // indirect
 	golang.org/x/mod v0.21.0 // indirect
 	golang.org/x/net v0.34.0 // indirect
