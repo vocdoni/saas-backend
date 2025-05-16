8d5b857f072a90546cc1bec27db9ff642b5bf355
emmdim <emmdim@users.noreply.github.com>
2025-05-16 16:44:23 +0200

    api: Implements /organizations/{org}/support endpoint that forwards user tickets to a support mail

--- api/api.go
+++ api/api.go
@@ -239,6 +239,9 @@ func (a *API) initRouter() http.Handler {
 		// delete a set of organization participants
 		log.Infow("new route", "method", "DELETE", "path", organizationDeleteParticipantsEndpoint)
 		r.Delete(organizationDeleteParticipantsEndpoint, a.deleteOrganizationParticipantsHandler)
+		// create a new ticket for the organization
+		log.Infow("new route", "method", "POST", "path", organizationCreateTicketEndpoint)
+		r.Post(organizationCreateTicketEndpoint, a.organizationCreateTicket)
 		// handle stripe checkout session
 		log.Infow("new route", "method", "POST", "path", subscriptionsCheckout)
 		r.Post(subscriptionsCheckout, a.createSubscriptionCheckoutHandler)
