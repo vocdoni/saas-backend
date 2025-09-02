a7b3aab91553899058619054ef0e748237bb4790
Guido Iribarren <git@guidoi.com.ar>
2025-09-02 11:46:22 +0200

    fix plan

--- api/api_test.go
+++ api/api_test.go
@@ -59,6 +59,83 @@ const (
 	testOAuthServiceURL = "http://test-oauth-service"
 )
 
+var mockPlans = []*db.Plan{
+	{
+		ID:            1,
+		Name:          "Free Plan",
+		StripeID:      "prod_test_free",
+		StripePriceID: "price_test_free",
+		StartingPrice: 0,
+		Default:       true,
+		Organization: db.PlanLimits{
+			Users:        1,
+			SubOrgs:      0,
+			MaxProcesses: 10,
+			MaxCensus:    50,
+			MaxDuration:  7,
+			CustomURL:    false,
+			Drafts:       0,
+		},
+		VotingTypes: db.VotingTypes{
+			Single:     true,
+			Multiple:   true,
+			Approval:   true,
+			Cumulative: true,
+			Ranked:     true,
+			Weighted:   true,
+		},
+		Features: db.Features{
+			Anonymous:       true,
+			Overwrite:       true,
+			LiveResults:     true,
+			Personalization: false,
+			EmailReminder:   false,
+			SmsNotification: false,
+			WhiteLabel:      false,
+			LiveStreaming:   false,
+			PhoneSupport:    false,
+		},
+		CensusSizeTiers: []db.PlanTier{},
+	},
+	{
+		ID:            2,
+		Name:          "Premium Annual Subscription Plan",
+		StripeID:      "prod_test_premium",
+		StripePriceID: "price_test_premium",
+		StartingPrice: 49900,
+		Default:       false,
+		Organization: db.PlanLimits{
+			Users:        5,
+			SubOrgs:      1,
+			MaxProcesses: 20,
+			MaxCensus:    10000,
+			MaxDuration:  90,
+			CustomURL:    false,
+			Drafts:       5,
+		},
+		VotingTypes: db.VotingTypes{
+			Single:     true,
+			Multiple:   true,
+			Approval:   true,
+			Cumulative: true,
+			Ranked:     false,
+			Weighted:   true,
+		},
+		Features: db.Features{
+			Anonymous:       true,
+			Overwrite:       true,
+			LiveResults:     true,
+			Personalization: true,
+			EmailReminder:   false,
+			SmsNotification: false,
+			WhiteLabel:      true,
+			LiveStreaming:   true,
+			PhoneSupport:    true,
+		},
+		CensusSizeTiers: []db.PlanTier{},
+	},
+}
+
 // apiError mimics the anon struct used in errors/errors.go
 // TODO: refactor that anon struct into an importable struct somehow
 type apiError struct {
@@ -183,47 +260,7 @@ func TestMain(m *testing.M) {
 	// set reset db env var to true
 	_ = os.Setenv("VOCDONI_MONGO_RESET_DB", "true")
 	// create a new MongoDB connection with the test database
-	// Create a simple default plan for tests
-	plans := []*db.Plan{
-		{
-			ID:            1,
-			Name:          "Free Plan",
-			StartingPrice: 0,
-			StripeID:      "prod_test_free",
-			StripePriceID: "price_test_free",
-			Default:       true,
-			Organization: db.PlanLimits{
-				Users:        1,
-				SubOrgs:      0,
-				MaxProcesses: 1,
-				MaxCensus:    100,
-				MaxDuration:  30,
-				CustomURL:    false,
-				Drafts:       1,
-			},
-			VotingTypes: db.VotingTypes{
-				Single:     true,
-				Multiple:   false,
-				Approval:   false,
-				Cumulative: false,
-				Ranked:     false,
-				Weighted:   false,
-			},
-			Features: db.Features{
-				Anonymous:       false,
-				Overwrite:       false,
-				LiveResults:     true,
-				Personalization: false,
-				EmailReminder:   false,
-				SmsNotification: false,
-				WhiteLabel:      false,
-				LiveStreaming:   false,
-				PhoneSupport:    false,
-			},
-			CensusSizeTiers: []db.PlanTier{},
-		},
-	}
-	if testDB, err = db.New(mongoURI, test.RandomDatabaseName(), plans); err != nil {
+	if testDB, err = db.New(mongoURI, test.RandomDatabaseName(), mockPlans); err != nil {
 		panic(err)
 	}
 	defer testDB.Close()
